990c2cdb07d36fa139f9537708029e046c335300
Da-max <da-max@tutanota.com>
2020-04-11 13:48:07 +0200

    Patch error in models

--- stats/models.py
+++ stats/models.py
@@ -11,7 +11,7 @@ class PageAccess (Model):
 
     """
 
-    name = CharField(max_length=255, verbose_name="Nom de la page d'accès")
+    name = CharField(max_length=255, verbose_name="Nom de la règle d’accès")
     url = URLField(verbose_name="Lien de la page",
                    help_text="Rentrer ici le lien de la page pour lequel il faut gérer l'accès.", unique=True)
     access = BooleanField(default=True, verbose_name="Autoriser l'accès",
@@ -19,7 +19,7 @@ class PageAccess (Model):
     raise_exception = BooleanField(default=False, verbose_name='Lever une exception',
                                    help_text="Cette case n’a d’importance que si l’accès à la page est bloqué. Dans ce cas là, cocher cette case empêchera réellement l’accès à la page, en la déclarant introuvable.")
     title = CharField(verbose_name="Titre de la fenêtre",
-                      help_text="Titre affiché sur la fenêtre empéchant l’accès à la page.", default="Vous êtes en avance (ou en retard) !", max_length=255)
+                      help_text="Titre affiché sur la fenêtre empêchant l’accès à la page.", default="Vous êtes en avance (ou en retard) !", max_length=255)
     content = RichTextField(verbose_name="Message affiché", help_text="Message affiché sur la fenêtre empêchant l’accès à la page.",
                         default="Cette partie du site n'est pas encore ouverte, ou est fermée temporairement. Merci de repasser plus tard ou de contacter un gérant du site si vous rencontrez un problème.", max_length=500)
     start_date = DateTimeField(verbose_name="Date de prise en compte", default=timezone.now,

061da60faea6d2d8d6a98aa45a59eeba0396806b
Da-max <da-max@tutanota.com>
2020-04-13 09:46:18 +0200

    Delete unused views in citrus api.

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from django.contrib.auth.models import User
 
-from api.views import CommandViewSet, AmoutViewSet, ProductViewSet, CurrentUserView, CoffeeViewSet, CommandCoffeeViewSet
+from api.views import CommandViewSet, ProductViewSet, CurrentUserView, CoffeeViewSet, CommandCoffeeViewSet
 from citrus.models import Amount, Product
 
 urlpatterns = [
@@ -15,7 +15,6 @@ urlpatterns = [
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'citrus/command', CommandViewSet)
-router.register(r'citrus/amount', AmoutViewSet, basename='amount')
 router.register(r'citrus/product', ProductViewSet, basename='product')
 router.register(r'coffee/coffee', CoffeeViewSet, basename='coffee-coffee')
 router.register(r'coffee/command', CommandCoffeeViewSet, basename='coffee-command-coffee')

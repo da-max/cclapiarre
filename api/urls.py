e6484d84025a145a1364a649eb02ead562d54ae9
Da-max <da-max@tutanota.com>
2020-04-15 19:16:34 +0200

    Add api/citrus/product?query=all in api urls.

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,8 @@ from api.views import CommandViewSet, ProductViewSet, CurrentUserView, CoffeeVie
 from citrus.models import Amount, Product
 
 urlpatterns = [
-    url(r'^users/current', CurrentUserView.as_view())
+    url(r'^users/current', CurrentUserView.as_view()),
+    url(r'^citrus/product\?query=all', ProductViewSet.as_view({'get': 'list_all'}))
 
 ]
 
@@ -17,6 +18,7 @@ router = DefaultRouter(trailing_slash=False)
 router.register(r'citrus/command', CommandViewSet)
 router.register(r'citrus/product', ProductViewSet, basename='product')
 router.register(r'coffee/coffee', CoffeeViewSet, basename='coffee-coffee')
-router.register(r'coffee/command', CommandCoffeeViewSet, basename='coffee-command-coffee')
+router.register(r'coffee/command', CommandCoffeeViewSet,
+                basename='coffee-command-coffee')
 
 urlpatterns += router.urls

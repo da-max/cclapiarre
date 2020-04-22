6886d7804e9f8c087700260339b440c09bb437fa
Da-max <da-max@tutanota.com>
2020-04-22 12:49:36 +0200

    Adding Breadcrumd in new views.

--- frontend/src/views/CitrusProductList.vue
+++ frontend/src/views/CitrusProductList.vue
@@ -1,6 +1,7 @@
 <template lang="fr">
     <div id='citrus-product-list'>
         <h1 class="uk-text-center uk-margin-large-bottom">Liste des produits</h1>
+        <breadcrumd :elements='breadcrumb'></breadcrumd>
         <aside class="uk-margin-large-top uk-margin-large-bottom uk-tile uk-tile-default uk-width-3-5@m uk-margin-auto uk-box-shadow-medium uk-text-justify">
             Ci-dessous se trouve la liste des produits disponibles à la commande sur la page <a href="/agrumes/commander-des-agrumes">commander des agrumes</a>. 
             Si les produits ont le status « afficher le produit » le produit sera affiché sur le tableau et les adhérents pourront le commander. Sinon, le produit 
@@ -12,6 +13,8 @@
 
 <script>
 import ListProduct from '../components/citrus/ListProduct'
+import Breadcrumd from '../components/utility/Breadcrumb'
+
 
 export default {
     name: "CitrusProductList",
@@ -20,8 +23,28 @@ export default {
         page: {type: Number, default() { return 1}}
     },
 
+    data() {
+        return {
+            breadcrumb: [
+                {
+                    name: 'Accueil',
+                    link: '/'
+                },
+                {
+                    name: 'Agrumes',
+                    link: '#',
+                    class: 'uk-disabled'
+                },
+                {
+                    name: 'Liste des produits'
+                }
+            ]
+        }
+    },
+
     components: {
-        ListProduct
+        ListProduct,
+        Breadcrumd
     },
 };
 </script>
--- frontend/src/views/CitrusProductUpdate.vue
+++ frontend/src/views/CitrusProductUpdate.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="citrus-product-update">
         <h1 class="uk-text-center">Modifier un produit</h1>
+        <breadcrumd :elements='breadcrumb'></breadcrumd>
         <layout-boxe>
             <template #body>
                 Ci-dessous se trouve un formulaire afin de modifier un produit, afin de valider la modification, penser à cliquer sur le bouton 
@@ -17,13 +18,38 @@
 <script>
 import LayoutBoxe from '../components/utility/LayoutBoxe'
 import UpdateProduct from '../components/citrus/UpdateCitrus'
+import Breadcrumd from '../components/utility/Breadcrumb'
 
 export default {
 	name: "CitrusProductUpdate",
 
     components: {
         LayoutBoxe,
-        UpdateProduct
-    }
+        UpdateProduct,
+        Breadcrumd
+    },
+    
+    data() {
+        return {
+            breadcrumb: [
+                {
+                    name: 'Accueil',
+                    link: '/'
+                },
+                {
+                    name: 'Agrumes',
+                    link: '#',
+                    class: 'uk-disabled'
+                },
+                {
+                    name: 'Liste des produits',
+                    link: '/agrumes/liste-des-produits'
+                },
+                {
+                    name: 'Modifier un produit'
+                }
+            ]
+        }
+    },
 };
 </script>

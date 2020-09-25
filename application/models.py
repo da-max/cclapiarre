1bcb9a937256ed205823384ec450df78d2bf565d
Da-max <da-max@tutanota.com>
2020-09-25 19:37:40 +0200

    feat(backend): start to write application models.

--- application/models.py
+++ application/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    """ Application model, this model is use by many of the following models
+    because it describes the core of the created application. 
+
+
+    """
+    name = models.CharField(verbose_name="Nom de l’application", max_lenght=60)

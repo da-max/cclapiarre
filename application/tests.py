f9c2c6cd22b0ea1994cbb86ce193bbe1032347fe
Da-max <da-max@tutanota.com>
2020-10-06 10:50:37 +0200

    feat(backend): write the first test for applation app

--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from graphene_django.utils.testing import GraphQLTestCase
+
+
+class ApplicationTestCase(GraphQLTestCase):
+    def test_all_applications_query(self):
+        response = self.query(
+            '''
+      query {
+        allApplications {
+          id,
+          name,
+          table
+        }
+      }
+      ''')
+        self.assertEqual(response.status_code, 200)

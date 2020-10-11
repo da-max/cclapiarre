8b297d653c8b59c2282468c351a9f2225d154115
Da-max <da-max@tutanota.com>
2020-10-11 19:41:15 +0200

    refactor(backend): deletion of unnecessary items in settings

--- backend/settings.py
+++ backend/settings.py
@@ -181,10 +181,7 @@ WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
         'BUNDLE_DIR_NAME': '/bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'dist', 'webpack-stats.json'),
-        'POLL_INTERVAL': 0.1,
-        'TIMEOUT': None,
-        'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'dist', 'webpack-stats.json')
     }
 }
 

45976341036b68766fb3265d6ae7dd2673ef7a9d
Da-max <da-max@tutanota.com>
2020-10-28 08:36:29 +0100

    feat(frontend): add login page

---
+++ frontend/src/views/Registration/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="uk-text-center">Se connecter</h1>
+    <LoginSection />
+  </div>
+</template>
+
+<script>
+import LoginSection from '@/components/Registration/Login/LoginSection'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginSection
+  }
+}
+</script>

bf110ac0722890cf04875e738c9f167983aa1a27
Da-max <da-max@tutanota.com>
2020-10-28 18:55:00 +0100

    core(frontend): write compoent for login page

---
+++ frontend/src/components/Registration/Login/LoginSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/Registration/Login/LoginForm'
+
+export default {
+  name: 'LoginSection',
+  components: {
+    LoginForm
+  }
+}
+</script>

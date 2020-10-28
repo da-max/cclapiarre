7eb8e6c8a3524d2a2377dce7c54937c92aaa9a7e
Da-max <da-max@tutanota.com>
2020-10-28 08:36:29 +0100

    Revert "Revert "feat(frontend): add login page""
    This reverts commit becf0dd52ff7780541330576075df6213476a650.

---
+++ frontend/src/components/Registration/Login/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form action="#">
+      <FormInput
+        :type="username.type"
+        :name="username.name"
+        :label="username.label"
+        :value="username.value"
+        v-model="user.username"
+      />
+
+      button.uk-button
+    </form>
+  </div>
+</template>
+
+<script>
+import FormInput from '@/components/Utils/Form/FormInput'
+
+export default {
+  name: 'LoginForm',
+  components: {
+    FormInput
+  },
+  data () {
+    return {
+      username: {
+        type: 'text',
+        name: 'username',
+        label: 'nom d’utilisateur',
+        value: ''
+      },
+      user: {
+        username: ''
+      }
+    }
+  }
+}
+</script>

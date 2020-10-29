6b218b4f89d7f2fc7d46708d9320d0d92e99c2c5
Da-max <da-max@tutanota.com>
2020-10-29 11:53:00 +0100

    chore(frontend): setup graphql loader

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,4 @@
-const BundleTracker = require('webpack-bundle-tracker');
+const BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
   publicPath: 'http://0.0.0.0:8080',
@@ -19,5 +19,13 @@ module.exports = {
       .watchOptions({ poll: 1000 })
       .https(false)
       .headers({ 'Access-Control-Allow-Origin': ['*'] })
+
+    // Graphql Loader
+    config.module
+      .rule('graphql')
+      .test('/.(graphql|gql)$/')
+      .use('graphql-tag/loader')
+      .loader('graphql-tag/loader')
+      .end()
   }
-};
+}

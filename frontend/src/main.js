d4ce95263364e01818d79a31f5bcddbd91e8fad4
Da-max <da-max@tutanota.com>
2020-10-29 14:33:23 +0100

    chore(frontend): add composition-api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import VueComp from '@vue/composition-api'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
+
+Vue.use(VueComp)
 
 new Vue({
   router,

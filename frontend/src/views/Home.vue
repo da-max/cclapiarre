2964b76aad7803c3f4ad067ffb1cf55bfc09e7fc
Da-max <da-max@tutanota.com>
2020-10-29 20:15:30 +0100

    chore(frontend): update views for use composition api

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <main>
-    <Carousels :carousels='allCarousels'></Carousels>
+    <Carousels v-if="data.data" :carousels="data.data.allCarousels"></Carousels>
     <div
       uk-grid
       class="uk-margin-auto uk-width-4-5@xl uk-width-expands uk-position-relative"
     >
-      <div class="uk-width-2-3@l uk-width-auto">
-        Article
-      </div>
+      <div class="uk-width-2-3@l uk-width-auto">Article</div>
       <div class="uk-width-1-3@l uk-width-auto uk-text-justify uk-padding">
         <PresentationCard></PresentationCard>
       </div>
@@ -18,28 +16,23 @@
 <script>
 import PresentationCard from '@/components/Home/PresentationCard'
 import Carousels from '@/components/Home/Carousels'
-import gql from 'graphql-tag'
+import { useSetupTitle } from '@/composition/utils'
+import { useDataFetcher } from '@/composition/useDataFetcher'
+
+import CarouselAll from '@/graphql/Carousel/CarouselAll.gql'
 
 export default {
+  setup (props) {
+    useSetupTitle('Accueil')
+    const { error, data } = useDataFetcher({ query: CarouselAll })
+    return {
+      error,
+      data
+    }
+  },
   components: {
     Carousels,
     PresentationCard
-  },
-  data () {
-    return {
-      allCarousels: []
-    }
-  },
-  apollo: {
-    allCarousels: gql`query {
-      allCarousels {
-        id,
-        title,
-        position,
-        description,
-        image
-      }
-    }`
   }
 }
 </script>
--- frontend/src/views/Registration/Login.vue
+++ frontend/src/views/Registration/Login.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <h1 class="uk-text-center uk-margin-large-top uk-margin-large-bottom">Se connecter</h1>
+    <h1 class="uk-text-center uk-margin-large-top uk-margin-large-bottom">
+      {{ title }}
+    </h1>
     <LoginSection />
   </div>
 </template>
 
 <script>
 import LoginSection from '@/components/Registration/Login/LoginSection'
+import { useSetupTitle } from '@/composition/utils'
 
 export default {
-  name: 'Login',
+  setup (props) {
+    const title = 'Se connecter'
+    useSetupTitle(title)
+    return {
+      title
+    }
+  },
   components: {
     LoginSection
   }

7b381982ce9c6532240c187d7e800558960c1125
Da-max <da-max@tutanota.com>
2020-10-29 12:25:46 +0100

    chore(frontend): update router utils function

--- frontend/src/router/utils.js
+++ frontend/src/router/utils.js
@@ -1,16 +1,21 @@
 import store from '@/store/index'
 
+const CONNECTION_URL = '/compte/connexion'
+
 export function loginRequired (to, _from, next) {
   const currentUser = store.state.auth.currentUser
 
-  currentUser && currentUser.username ? next() : next(`/compte/connexion?next=${to.path}`)
+  currentUser && currentUser.username ? next() : next(`${CONNECTION_URL}?next=${to.path}`)
 }
 
 export function groupRequired (to, _from, next, groupRequired) {
-  loginRequired(to, _from, next)
   const currentUser = store.state.auth.currentUser
 
-  !currentUser.isSuperuser && currentUser.groups.filter(group => group.name === groupRequired)
-    ? next(`/compte/connexion?next=${to.path}`)
-    : next()
+  if (currentUser && currentUser.username) {
+    !currentUser.isSuperuser && currentUser.groups.filter(group => group.name === groupRequired)
+      ? next(`${CONNECTION_URL}?next=${to.path}`)
+      : next()
+  } else {
+    next(`${CONNECTION_URL}?next=${to.path}`)
+  }
 }

2bc97d70450ebc017f9e3872ebdb5db89813d171
Da-max <da-max@tutanota.com>
2020-10-29 11:54:42 +0100

    chore(frontend): separation of graphql call's in files

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,63 @@
+import { apolloClient } from '@/vue-apollo'
+
+import USER_LOGIN from '@/graphql/User/UserLogin.gql'
+import USER_LOGOUT from '@/graphql/User/UserLogout.gql'
+import USER_CURRENT from '@/graphql/User/UserCurrent.gql'
+
+export default {
+  namespaced: true,
+  state: () => {
+    return { currentUser: {} }
+  },
+
+  mutations: {
+    LOGOUT_USER (state) {
+      state.currentUser = {}
+    },
+    SET_CURRENT_USER (state, user) {
+      state.currentUser = user
+    }
+  },
+
+  actions: {
+    async logoutUser ({ commit }) {
+      try {
+        const response = await apolloClient.mutate({
+          mutation: USER_LOGOUT
+        })
+        commit('LOGOUT_USER')
+        return response.data.logout.ok
+      } catch {
+        return { error: 'Vous n’avez pas pu être déconnecté. Merci de réessayer.' }
+      }
+    },
+
+    async loginUser ({ commit }, { username, password }) {
+      try {
+        const response = await apolloClient.mutate({
+          mutation: USER_LOGIN,
+          variables: {
+            username,
+            password
+          }
+        })
+
+        const user = response.data.login.user
+        commit('SET_CURRENT_USER', user)
+        return response.data.login
+      } catch (e) {
+        return { error: 'Nom d’utilisateur ou mot de passe incorrect. Merci de réessayer.' }
+      }
+    },
+
+    /*
+    This action get user with the sessionid cookie if the user is logged.
+    */
+    async loadUser ({ commit }) {
+      const response = await apolloClient.query({
+        query: USER_CURRENT
+      })
+      commit('SET_CURRENT_USER', response.data.user)
+    }
+  }
+}
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,64 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import gql from 'graphql-tag'
-import { apolloClient } from '@/vue-apollo'
 
+import AuthModule from '@/store/auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    currentUser: {}
   },
   mutations: {
-    LOGOUT_USER (state) {
-      state.currentUser = {}
-      window.localStorage.currentUser = JSON.stringify({})
-    },
-    SET_CURRENT_USER (state, user) {
-      state.currentUser = user
-      window.localStorage.currentUser = JSON.stringify(user)
-    }
+
   },
+
   actions: {
-    logoutUser ({ commit }) {
-      commit('LOGOUT_USER')
-    },
-    async loginUser ({ commit }, { username, password }) {
-      try {
-        const response = await apolloClient.mutate({
-          mutation: gql`mutation ($username: String!, $password: String!) {
-            login(username: $username, password: $password) {
-              token,
-              user {
-                id
-                username
-                email
-                isSuperuser
-                groups {
-                  id
-                  name
-                }
-              }
-            }
-          }`,
-          variables: {
-            username,
-            password
-          }
-        })
-        console.log(response)
-        commit('SET_CURRENT_USER', response.data.login.user)
-        return response.data.login
-      } catch (e) {
-        console.log(e)
-        return { error: 'Username/password combinaison was incorrect. Please try again.' }
-      }
-    },
-    loadUser ({ commit }) {
-      const user = JSON.parse(window.localStorage.currentUser)
-      commit('SET_CURRENT_USER', user)
-    }
+
   },
   modules: {
+    auth: AuthModule
   }
 })

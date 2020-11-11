5398276d4461b784b0731a43615cfd50410718cb
Da-max <da-max@tutanota.com>
2020-11-11 09:48:21 +0100

    refactor(backend): delete unsued signal and update decorators

--- backend/registration/decorators.py
+++ backend/registration/decorators.py
@@ -5,64 +5,25 @@ from django.contrib.auth.models import User
 from backend.application.models import Application
 
 
-def login_required(func):
-    def wrapper(root, info, *args, **kwargs):
-        if info.context.user.is_anonymous:
-            raise PermissionDenied('We must be logged to access this data.')
-        return func(root, info, *args, **kwargs)
-    return wrapper
-
-
-# Thanks to
-# https://medium.com/@anish.shrestha/relay-graphene-django-and-permissions-b1b2b96daf20 for exemple.
-def permissions_required(perm):
+def check_application_permission_by_slug(perm='admin'):
     def decorator(func):
-        def wrapper(root, info, **kwargs):
-            user = info.context.user
-            if isinstance(perm, str):
-                perms = (perm, )
-            else:
-                perms = perm
+        def wrapper(root, info, *args, **kwargs):
+            if info.context.user.is_superuser:
+                return func(root, info, args, kwargs)
 
-            if user.has_perms(perms):
-                return func(root, info, **kwargs)
-
-            raise PermissionDenied("Permission Denied")
-
-        return wrapper
-    return decorator
-
-
-def application_permissions_required(group_type="admins"):
-    """ 
-    Params : perm table or string
-    Description : This decorator is used to check if the user has 
-    permission under the form Admin/Member <application_name>.
-    """
-    def decorator(func):
-        def wrapper(root, info, application__slug, *args, **kwargs):
-            user = info.context.user
             try:
-                app = Application.objects.get(slug=application__slug)
-            except Application.DoesNotExist:
-                raise Application.DoesNotExist("Application not found.")
+                if perm == 'member':
+                    assert info.context.user.member_application.filter(
+                        slug=kwargs['application__slug'])
+                else:
+                    assert info.context.user.admin_application.filter(
+                        slug=kwargs['application__slug'])
 
-            if group_type == "members":
-                try:
-                    assert user.is_superuser or app.admins.get(username=user.username) or app.members.get(
-                        username=user.username)
-                except AssertionError:
-                    raise PermissionDenied("Permission Denied")
+            except AssertionError:
+                raise PermissionDenied(
+                    "You dont have permission to access to this resource.")
 
-            else:
-                try:
-                    assert user.is_superuser or app.admins.get(
-                        username=user.username)
-
-                except AssertionError:
-                    raise PermissionDenied("Permission Denied")
-
-            return func(root, info, application__slug, *args, **kwargs)
+            return func(root, info, args, kwargs)
 
         return wrapper
     return decorator

642e73ebce690e00c2cdd1cafd442b6fe6679d20
Da-max <da-max@tutanota.com>
2020-12-18 11:04:07 +0100

    chore(frontend): add CoffeeList in OrderSection

--- frontend/src/components/Coffee/Order/CoffeeOrderSection.vue
+++ frontend/src/components/Coffee/Order/CoffeeOrderSection.vue
@@ -1,16 +1,19 @@
 <template>
-  <section>
+  <section class="uk-margin-medium-top">
     <OrderInformation class="uk-width-1-2@l uk-margin-auto" />
+    <CoffeeList></CoffeeList>
   </section>
 </template>
 
 <script>
 import OrderInformation from '@/components/Application/Order/Section/OrderInformation'
+import CoffeeList from '@/components/Coffee/Order/Section/Coffee/CoffeeList'
 
 export default {
   name: 'CoffeeOrderSection',
   components: {
-    OrderInformation
+    OrderInformation,
+    CoffeeList
   }
 }
 </script>
--- frontend/src/views/Coffee/Order.vue
+++ frontend/src/views/Coffee/Order.vue
@@ -6,12 +6,20 @@
 </template>
 
 <script>
+import useCoffee from '@/composition/coffee/useCoffee'
+
 import CoffeeOrderHeader from '@/components/Coffee/Order/CoffeeOrderHeader.vue'
 import CoffeeOrderSection from '@/components/Coffee/Order/CoffeeOrderSection'
+
 export default {
+  name: 'CoffeeOrder',
   components: {
     CoffeeOrderHeader,
     CoffeeOrderSection
+  },
+  setup (props) {
+    const { getCoffees } = useCoffee()
+    getCoffees()
   }
 }
 </script>

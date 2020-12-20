70d602f4d915bbe3d18c20e365054f9ae2262226
Da-max <da-max@tutanota.com>
2020-12-20 11:24:59 +0100

    refactor(frontend): delete unused code

--- frontend/src/composition/application/useOrder.js
+++ frontend/src/composition/application/useOrder.js
@@ -1,6 +1,6 @@
 import store from '@/store/index'
 
-export default function (emit) {
+export default function () {
   const addProductOrder = (product) => {
     // eslint-disable-next-line no-undef
     UIkit.scroll('#addProductButton', { offset: 100 }).scrollTo('#order-list')

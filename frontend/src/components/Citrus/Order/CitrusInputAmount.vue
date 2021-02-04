f84febd701db19b8dd902c2607d4fbdfe122dfcc
da-max <da-max@tutanota.com>
2021-02-04 11:18:05 +0100

    chore(frontend): create CitrusInputAmount component for better organization

---
+++ frontend/src/components/Citrus/Order/CitrusInputAmount.vue
@@ -0,0 +1,51 @@
+<template>
+  <FormInputNumber
+    :name="product.name"
+    :value="amount"
+    label="Quantité commandé"
+    :display-info="false"
+    :display-label="false"
+    :step="product.step"
+    :max="product.maximum"
+    @input="changeOrderAmount"
+  />
+</template>
+
+<script>
+import useOrder from '@/composition/citrus/useOrder'
+
+import FormInputNumber from '@/components/Utils/Form/FormInputNumber'
+
+export default {
+    name: 'CitrusInputAmount',
+    components: {
+        FormInputNumber
+    },
+    props: {
+        product: {
+            required: true,
+            type: Object
+        },
+        amount: {
+            required: false,
+            type: Number,
+            default: 0
+        }
+    },
+    setup (props) {
+        const { setCurrentOrderAmount } = useOrder()
+
+        const changeOrderAmount = (newAmount) => {
+            setCurrentOrderAmount(props.product.id, newAmount)
+        }
+
+        return {
+            changeOrderAmount
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

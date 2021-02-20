5979dc4f1288ec96e524c6cdce819db9388f4f86
da-max <da-max@tutanota.com>
2021-02-20 10:22:29 +0100

    chore(frontend): added an alert when the user has already ordered

--- frontend/src/views/Citrus/Order.vue
+++ frontend/src/views/Citrus/Order.vue
@@ -5,6 +5,21 @@
       class="uk-width-2-5@l uk-width4-5@s uk-margin-auto uk-margin-large-bottom"
     >
       <Alerts />
+      <Alert
+        v-show="hasOrder"
+        :alert-id="Date.now()"
+        :header="true"
+        type="primary"
+      >
+        <template #header>
+          Vous avez commandé
+        </template>
+        <template #body>
+          Vous avez commandé, si vous souhaitez modifier votre commande
+          merci de contacter la responsable des commandes à cette
+          adresse : valerie.lechateau@gmail.com
+        </template>
+      </Alert>
     </div>
     <CitrusOrderSection />
   </main>
@@ -12,20 +27,25 @@
 
 <script>
 import { useSetupTitle } from '@/composition/useUtils'
+import useOrder from '@/composition/citrus/useOrder'
 
 import Alerts from '@/components/Utils/Alert/Alerts'
 import CitrusOrderHeader from '@/components/Citrus/Order/CitrusOrderHeader'
 import CitrusOrderSection from '@/components/Citrus/Order/CitrusOrderSection'
+import Alert from '../../components/Utils/Alert/Alert.vue'
 
 export default {
     name: 'Order',
     components: {
+        Alert,
         Alerts,
         CitrusOrderHeader,
         CitrusOrderSection
     },
     setup () {
         useSetupTitle('Commander des agrumes')
+        const { hasOrder } = useOrder()
+        return { hasOrder }
     }
 }
 </script>

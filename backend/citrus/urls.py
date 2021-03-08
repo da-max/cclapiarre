a1ab0ef30993b1d9ad5dd3edec22ec0fb285ff27
da-max <da-max@tutanota.com>
2021-03-08 16:32:45 +0100

    refactor(backend): refactor sommry_orders view

--- backend/citrus/urls.py
+++ backend/citrus/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path, include
 from django.conf.urls import url
 
-from backend.citrus.views import sommary_command
+from backend.citrus.views import sommary_orders
 
 urlpatterns = [
-    path("recapitulatif-de-la-commande", sommary_command, name='sommary_command')
+    path("recapitulatif-de-la-commande", sommary_orders, name='sommary_orders')
 ]
--- backend/citrus/views.py
+++ backend/citrus/views.py
@@ -12,45 +12,24 @@ from backend.citrus.models import CitrusProduct, CitrusOrder, CitrusAmount
 @login_required
 @permission_required('citrus.change_product', raise_exception=True)
 @pdf_decorator(pdfname="recapitulatif_de_la_commande.pdf")
-def sommary_command(request):
-    amouts_list = dict()
-    users = list()
-    command_user = dict()
-    products_list = dict()
-    total = float()
-    user_total = float()
+def sommary_orders(request):
+    orders_list = dict()
 
     products = CitrusProduct.objects.filter(display=True)
-    commands = CitrusOrder.objects.all()
-    amouts = CitrusAmount.objects.all()
+    orders = CitrusOrder.objects.all()
+    amounts = CitrusAmount.objects.all()
+    total = 0
 
-    for command in commands:
-
-        user_total = CitrusAmount.get_total_user(CitrusAmount, command.id)
-        users.append((command.user.username, user_total))
-        total += user_total
-
-    for product in products:
-        command_user[product.name] = []
-        amouts_list[product.name] = []
-        for amout in amouts.filter(product=product):
-            amouts_list[product.name].append({'user': amout.command.user.username,
-                                              'quantity': amout.amount})
-            command_user[product.name] += amout.command.user.username
-
-        products_list[product.id] = {
-            'name': product.name,
-            'description': product.description,
-            'price': product.price,
-            'weight': product.weight,
-            'step': str(product.step).replace(',', '.'),
-            'maximum': product.maximum,
-            'amouts': amouts_list[product.name],
-            'users': command_user,
-            'total': CitrusAmount.get_total_product(CitrusAmount, product)
-        }
-
-    return render(request, "citrus/pdf/sommary_command.html", {'products_list': products_list, 'users': users, 'total': total})
+    for order in orders:
+        orders_list[order.user.username] = (
+            amounts.filter(order=order), order.get_price())
+        total += order.get_price()
+    print(orders_list)
+    return render(
+        request,
+        "citrus/pdf/sommary_orders.html",
+        {'total': round(total, 2), 'orders': orders_list, 'products': products}
+    )
 
 
 # Signals
@@ -79,5 +58,5 @@ def send_confirm_citrus_order_mail(sender, order: CitrusOrder, create: bool = Tr
             'amounts': order.product.through.objects.filter(order=order),
             'create': create
         })
-    
+
         async_send_mail('Commande confirmée', html_content, [order.user.email])

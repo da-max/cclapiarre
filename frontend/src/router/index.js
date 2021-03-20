89fefb73b4c9a5be5fc0e8f4c1b8594a4a4e37f7
da-max <da-max@tutanota.com>
2021-03-20 16:31:51 +0100

    chore(frontend/auth): adding permission for memberlist view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -23,18 +23,13 @@ const routes = [
         component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
         path: '/compte/liste-des-adherents',
         name: 'MemberList',
         component: () => import(/* webpackChunkName: "member-list" */ '../views/Registration/MemberList.vue'),
-        meta: { loginRequired: true }
+        meta: {
+            loginRequired: true,
+            permissionRequired: 'view_information'
+        }
     },
     {
         path: '/compte/connexion',

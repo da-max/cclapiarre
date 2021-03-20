0a9d9a32749d55e401389fdb6f31469afe82d28a
da-max <da-max@tutanota.com>
2021-03-20 15:35:51 +0100

    chore(frontend/auth): adding citrus permissions in router

--- frontend/src/router/citrus.js
+++ frontend/src/router/citrus.js
@@ -3,12 +3,18 @@ export default [
         path: '/agrumes/liste-des-produits',
         name: 'CitrusList',
         component: () => import(/* webpackChunkName "citrus-list" */'../views/Citrus/ProductList.vue'),
-        meta: { loginRequired: true }
+        meta: {
+            loginRequired: true,
+            permissionRequired: 'view_citrusproduct'
+        }
     },
     {
         path: '/agrumes/commander',
         name: 'CitrusOrder',
         component: () => import(/* webpackChunkName "citrus-order" */'../views/Citrus/Order'),
-        meta: { loginRequired: true }
+        meta: {
+            loginRequired: true,
+            permissionRequired: 'add_citrusorder'
+        }
     }
 ]

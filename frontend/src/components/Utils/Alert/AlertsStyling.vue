3d905ebd336c5dd969c5ab85695618fb7980f99a
da-max <da-max@tutanota.com>
2021-03-21 22:33:41 +0100

    chore(frontend): creating AlertsStyling component for adding style to alerts container

---
+++ frontend/src/components/Utils/Alert/AlertsStyling.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="uk-width-2-5@l uk-width4-5@s uk-margin-auto uk-margin-large-bottom"
+  >
+    <Alerts />
+  </div>
+</template>
+
+<script>
+import Alerts from '@/components/Utils/Alert/Alerts'
+
+export default {
+    name: 'AlertsStyling',
+    components: {
+        Alerts
+    }
+}
+</script>

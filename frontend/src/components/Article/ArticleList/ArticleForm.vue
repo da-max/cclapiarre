7e5763439f10fa2f40289fc903c1102a70ef47ca
da-max <da-max@tutanota.com>
2021-03-29 14:06:59 +0200

    feat(frontend): create ArticleForm component for update or create an article

---
+++ frontend/src/components/Article/ArticleList/ArticleForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="uk-for-horizontal">
+    <FormInput
+      v-model="article.title"
+      name="title"
+      label="Titre de l’article"
+      :value="article.title"
+    />
+    <div class="uk-margin-medium-top">
+      <label
+        for="description"
+        class="uk-form-label"
+      >Contenu de l’article</label>
+      <ckeditor
+        id="description"
+        v-model="article.content"
+        :value="article.content"
+        required="true"
+        :editor="editor"
+      />
+    </div>
+  </form>
+</template>
+<script>
+import useArticle from '@/composition/article/useArticle'
+
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from 'ckeditor5-build-classic-with-font'
+
+import FormInput from '@/components/Utils/Form/FormInput.vue'
+
+export default {
+    name: 'ArticleForm',
+    components: {
+        FormInput,
+        ckeditor: CKEditor.component
+    },
+    setup () {
+        const { article } = useArticle()
+
+        return {
+            article,
+            editor: ClassicEditor
+        }
+    }
+
+}
+</script>
